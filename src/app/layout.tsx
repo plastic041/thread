9ffbbc327ccba47e661c627e1134f8b0e1b827fe
plastic041 <plastic041@gmail.com>
2024-12-24 18:15:46 +0900

    name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Instacopy",
+  title: "Thread",
   description: "Basic Instagram clone",
 };
 
